33662b3d6428396336b6b975d7973ef20c555c8e
Mohtimad <dam.thomas.00@gmail.com>
2021-06-10 17:59:48 +0200

    add product page and update index

--- scripts/home.js
+++ scripts/home.js
@@ -2,18 +2,15 @@ function uptadetCards(value) {
     const elt = document.getElementById("card-list");
     const numberOfItems = value.length;
     for (let i = 0; i < numberOfItems; i++) {
-      elt.innerHTML += `<div class= "col-12 col-sm-6 col-lg-${2 + (i % 2)} card-group">
-                          <div class="card">
-                              <div class="card-header  text-center">
+      elt.innerHTML += `<div class= "col-12 col-sm-6 col-lg-${2 + (i % 2)} card-group mb-4">
+                          <div class="card shadow">
+                              <h2 class="card-header fw-bold text-center h5 text-nowrap">
                                 ${value[i].name}
-                              </div>
+                              </h2>
                               <a href="products.html?id=${value[i]._id}" class="stretched-link text-danger" style="position: relative;">
                                 <img class="card-img-top" src="${value[i].imageUrl}" alt="Caméra ${value[i].name}">
                               </a>
-                              <div class="card-img-overlay">
-                                <h2 class="h5"><span class="badge bg-info text-dark"></span></h2>
-                              </div>
-                              <div class="card-footer text-muted text-right">
+                              <div class="card-footer text-muted text-right ">
                                 <p class="price font-family--Lobster">${(value[i].price / 100).toFixed(2)}€</p>
                               </div>
                             </div>
@@ -33,4 +30,5 @@ fetch("http://localhost:3000/api/cameras")
   })
   .catch(function(err) {
     // Une erreur est survenue
+    windows.alert(err)
   });
